14703299457ca4eb7b22040a24c0712b246701df
PAULA EDUARDA DE LIMA <paulaeduardalima09@gmail.com>
2023-09-18 14:42:36 -0300

    Crie arquivo com as funções rascunhos

---
+++ Rascunhos.py
@@ -0,0 +1,25 @@
+# #data final 
+# d2 = datetime.strptime("2017-05-05", "%Y-%m-%d")
+
+# d1 = datetime.strptime("2017-05-01", "%Y-%m-%d")
+
+# print(quant_dias = abs( ( d2 - d1 ).days))
+
+"""
+
+def diferenca_dias():
+    datas = input("Digite a data inicial e a data final: ")27 de março de 2023 - 18 de agosto de 2021
+    
+    data_final = datas.strip("-")
+
+
+    # data = datetime.strptime(datas, "%Y-%m-%d")
+
+    # diferenca_de_dias = abs( (datas).days)
+
+    return data_final
+
+print(diferenca_dias())
+
+"""
+
