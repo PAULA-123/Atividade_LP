8c3a6deadb7dc1e45d82fc984989fb2c5bf9abf4
Ana Júlia Amaro Pereira Rocha <B51991@fgv.edu.br>
2023-09-19 09:51:17 -0300

    tentando reduzir os códigos contidos na main

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from kivymd.app import MDApp
-from kivymd.uix.floatlayout import MDFloatLayout
+# from kivymd.app import MDApp
+# from kivymd.uix.floatlayout import MDFloatLayout
+# import numero_dias.py as num_dias
+from calculadora_de_dias import TelaPython
 
-class Tela(MDFloatLayout):
-    pass
+tela = TelaPython()
+tela.Iniciar()
 
-class Calculadora_de_dias(MDApp):
-    pass
+# class Calculadora_de_dias(MDApp):
+#     pass
 
-if __name__=="__main__":
-    Calculadora_de_dias().run()
+# if __name__=="__main__":
+#     Calculadora_de_dias().run()
