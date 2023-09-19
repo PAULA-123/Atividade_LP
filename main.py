10eb555d4510fa59e4681b7ab07c38e2427a6fdc
Ana Júlia Amaro Pereira Rocha <B51991@fgv.edu.br>
2023-09-19 10:59:11 -0300

    deixando o código mais limpo

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-# from kivymd.app import MDApp
-# from kivymd.uix.floatlayout import MDFloatLayout
-# import numero_dias.py as num_dias
+import PySimpleGUI as sg
 from calculadora_de_dias import TelaPython
 
-tela = TelaPython()
-tela.Iniciar()
 
-# class Calculadora_de_dias(MDApp):
-#     pass
+if __name__=="__main__":
+    tela = TelaPython()
+    tela.Iniciar()
 
-# if __name__=="__main__":
-#     Calculadora_de_dias().run()
+
