9055876b19a9fb5973bfd3a600f14b43305a981a
Ana Júlia Amaro Pereira Rocha <B51991@fgv.edu.br>
2023-09-19 13:59:17 -0300

    aperfeiçoando o código para aceitar aquivo .txt como entrada também

--- calculadora_de_dias.py
+++ calculadora_de_dias.py
@@ -4,11 +4,11 @@ from numero_dias import diferenca_dias_input
 class TelaPython:
     def __init__(self):
         layout = [
-            [sg.Text("Digite S para sair ou insira as data no formato (dia de mês de ano - dia de mês de ano): ", size=(65,0)),sg.Input(size=(60,0),key="datas")],
+            [sg.Text("Digite S para sair, insira as data no formato (dia de mês de ano - dia de mês de ano) ou forneça um arquivo de texto com as datas nesse formato: ", size=(75,0)),sg.Input(size=(70,0),key="entrada")],
             [sg.Button("Enviar Dados", size=(20,0)), sg.Button("Sair", size=(6,0))], #adicionando um botão para sair
-            [sg.Output(size=(30,20))]
+            [sg.Output(size=(40,30))]
         ]
-        self.janela = sg.Window("Calculadora de Dias", layout, finalize=True)
+        self.janela = sg.Window("Calculadora de Dias", layout, finalize=True, size=(1200,600))
         
     def Iniciar(self):
         while True:
@@ -16,6 +16,16 @@ class TelaPython:
             if event == sg.WINDOW_CLOSED or event == "Sair":
                 break
             elif event == "Enviar Dados":
-                datas = values["datas"]
-                print(diferenca_dias_input(datas))
+                entrada = values["entrada"]
+                if entrada.endswith(".txt"):
+                    try:
+                        with open(entrada, "r") as file:
+                            datas = file.readline()
+                            print(diferenca_dias_input(datas))
+                    except FileNotFoundError:
+                        print("Arquivo não encontrado!")
+                    except Exception:
+                        print(f"Erro ao processar o arquivo: {Exception}")
+                else:
+                    print(diferenca_dias_input(entrada))
         self.janela.close()
