a37a105bc0b3acb0b6d6e281fcffb2a3ab1be7f0
Ana Júlia Amaro Pereira Rocha <B51991@fgv.edu.br>
2023-09-19 21:05:15 -0300

    comentando o código que gera a janela do app

--- calculadora_de_dias.py
+++ calculadora_de_dias.py
@@ -2,30 +2,42 @@ import PySimpleGUI as sg
 from numero_dias import diferenca_dias_input
 
 class TelaPython:
+    # Definindo a função com as características da janela do meu app, seus botões, entrada e saída de dados.
     def __init__(self):
         layout = [
-            [sg.Text("Digite S para sair, insira as data no formato (dia de mês de ano - dia de mês de ano) ou forneça um arquivo de texto com as datas nesse formato: ", size=(75,0)),sg.Input(size=(70,0),key="entrada")],
-            [sg.Button("Enviar Dados", size=(20,0)), sg.Button("Sair", size=(6,0))], #adicionando um botão para sair
+            [sg.Text("insira as data no formato (dia de mês de ano - dia de mês de ano) ou forneça o nome de um arquivo de texto no qual as datas estejam escritas nesse formato: ", size=(75,0)),sg.Input(size=(70,0),key="entrada")],
+            [sg.Button("Enviar Dados", size=(20,0)), sg.Button("Sair", size=(6,0))],
             [sg.Output(size=(40,30))]
         ]
         self.janela = sg.Window("Calculadora de Dias", layout, finalize=True, size=(1200,600))
-        
+    
+    # Definindo a função com o loop principal do app, a qual roda enquanto o usuário estiver usando a plataforma.
     def Iniciar(self):
         while True:
             event, values = self.janela.read()
+            # caso o usuário clique no botão "Sair" ou feche a janela do app, o loop se encerra.
             if event == sg.WINDOW_CLOSED or event == "Sair":
                 break
+            # Se o usuário clicar no botão "Enviar Dados", deve-se checar se a entrada é um arquivo .txt ou não
             elif event == "Enviar Dados":
                 entrada = values["entrada"]
+                # Se for um arquivo .txt, abre no modo leitura, lê a primeira linha e usa os dados nela contidos para executar a função que calcula a diferença em dias.
                 if entrada.endswith(".txt"):
                     try:
                         with open(entrada, "r") as file:
                             datas = file.readline()
                             print(diferenca_dias_input(datas))
+                    # Tratando a exceção em que não se encontra o arquivo especificado pelo usuário
                     except FileNotFoundError:
                         print("Arquivo não encontrado!")
+                    # Tratando a exceção na qual o conteúdo do arquivo .txt não está no formato de data especificado
+                    except IndexError:
+                        print("Dados fornecidos com o formato incorreto!")
+                    # Tratando as demais exceções 
                     except Exception:
                         print(f"Erro ao processar o arquivo: {Exception}")
+                # Se não for um arquivo .txt, apenas executa a função que calcula a diferença em dias, usando os dados da entrada como parâmetro.
                 else:
                     print(diferenca_dias_input(entrada))
+        # Caso o que o usuário informe não seja nem um arquivo .txt e nem uma data com o formato correto para executar a função "diferenca_dias_input", a janela se fecha, encerrando o programa.
         self.janela.close()
